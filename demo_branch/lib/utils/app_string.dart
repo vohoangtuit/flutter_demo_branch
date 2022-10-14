9a21fbfe67fc47c27017086ac54c017cd32fc8cf
tuvh <tuvh.itc@vietravel.com>
2022-10-14 09:36:08 +0700

    add text file string

--- demo_branch/lib/utils/app_string.dart
+++ demo_branch/lib/utils/app_string.dart
@@ -5,4 +5,8 @@ class AppString{
   static String connected ='Đã kết nối';
   static String noConnected ='Không có kết nối';
   static String loadingDataTryAgain ='Đang tải dữ liệu, vui lòng thử lại sau';
+
+  static String vietnam ='Việt Nam';
+  static String provinceCity ='Tỉnh/Thành phố';
+  static String quanHuyen ='Quận/Huyện';
 }
